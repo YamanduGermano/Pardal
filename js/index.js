a9b2de749ad4c0529e970e79e7e79a70e17501cd
YamanduGermano <yamandu.germano@gmail.com>
2024-05-21 14:10:02 -0300

    config debugada

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     modoRua = this.querySelector("#modoRua")
+    iconeMR = this.querySelector('#modo')
+    overlay = this.querySelector('#overlay')
 
     mRStatus = localStorage.getItem('modoRuaStatus')
     if(mRStatus==null){
@@ -12,19 +14,34 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     modoRua.addEventListener('click', function(e) {
+
+        // Mudar variável de controle do Modo Rua
         mRStatus = !mRStatus
         console.log(mRStatus)
         localStorage.setItem('modoRuaStatus',mRStatus)
+        // Mudar imagem do icone do Modo Rua
+        overlay.classList.toggle('active')
         if (mRStatus){
             modoRua.querySelector('img').src = '../assets/Modo RuaAtivo.svg'
+            iconeMR.src = '../assets/iconeInativo.svg'
         } else {
             modoRua.querySelector('img').src = '../assets/Modo Rua.svg'
+            iconeMR.src = '../assets/iconeAtivo.svg'
         }
         e.stopPropagation()
+        setTimeout(overlay.classList.toggle('active'),1000)
+        console.log('a')
     })
 
     drawer = this.querySelector('#drawer')
     drawer.addEventListener('click',function(){
         drawer.classList.toggle('active')
     })
+
+
+    if (modoRua){
+        iconeMR.src = '../assets/iconeAtivo.svg'
+    } else{
+        iconeMR.src = '../assets/iconeInativo.svg'
+    }
 })
