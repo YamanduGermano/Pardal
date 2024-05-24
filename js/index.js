9dd46c84ef00e9806755ea2faa44a31fca009a58
YamanduGermano <yamandu.germano@gmail.com>
2024-05-23 23:21:17 -0300

    input de rota com animação

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     modoRua = this.querySelector("#modoRua")
     iconeMR = this.querySelector('#modo')
+    overlayModoRua = this.querySelector('#overlayModoRua')
     overlay = this.querySelector('#overlay')
     textoMR = this.querySelector('#texto_MR')
 
@@ -27,9 +28,11 @@ document.addEventListener('DOMContentLoaded', function() {
         if (mRStatus){
             modoRua.querySelector('img').src = '../assets/Modo RuaAtivo.svg';
             iconeMR.src = '../assets/iconeInativo.svg';
-            textoMR.innerHTML = "Modo rua Inativado"
+            textoMR.innerHTML = "Modo Rua desativado"
+            overlayModoRua.classList.add('active');
             overlay.classList.add('active');
             setTimeout(() => {
+                overlayModoRua.classList.remove('active');
                 overlay.classList.remove('active');
             }, 1000);
             
@@ -37,9 +40,11 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             modoRua.querySelector('img').src = '../assets/modo-Rua-Inativo.svg'
             iconeMR.src = '../assets/iconeAtivo.svg'
-            textoMR.innerHTML = "Modo rua ativado"
+            textoMR.innerHTML = "Modo rua Ativado"
+            overlayModoRua.classList.add('active');
             overlay.classList.add('active');
             setTimeout(() => {
+                overlayModoRua.classList.remove('active');
                 overlay.classList.remove('active');
             }, 1000);
             
@@ -129,5 +134,18 @@ document.addEventListener('DOMContentLoaded', function() {
         localStorage.setItem('pinIDs',JSON.stringify(pinIDs))
 
         e.stopPropagation()
+    })
+
+    input = this.querySelector('input')
+    navRotas = this.querySelector('#navRotas')
+    input.addEventListener('focus', (e)=>{
+        if (!('active' in navRotas.classList)){
+            navRotas.classList.add('active')
+        }
+    })
+
+    x = this.querySelector('#fecharRotas')
+    x.addEventListener('click',()=>{
+        navRotas.classList.remove('active')
     })
 })
